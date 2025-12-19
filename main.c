c1a1eeb8de55c50d5ce38fdb7f135f50722aaec2
mohamed-belk <mohreddz@gmail.com>
2025-12-19 16:03:02 +0100

    Create main.c
    Add main game code

---
+++ main.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+int main()
+{
+    printf("welcome in\n");
+ printf("Enchanted Forest Adventure\n");
+printf("do you want play this game\n");
+int x;
+printf("enter 1 for yes and 0 for no\n");
+scanf("%d",&x);
+if(x==1){
+    printf("good let's start\n");
+}else{
+ return 0;   
+}
+    
+}
